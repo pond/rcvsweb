0bd740aeabfbda2fe8a4ac2e85bd61f17febe452
adh <adh@fbfdfdf0-2b19-0410-bbe2-c4a1547f2368>
2006-11-14 22:39:20 +0000

    Various updates, mostly concerned with integration with cvslog2web for faster and more accurate log processing. The synthesised revision summaries from CVSHistory remain though the user is shown cvslog2web output by default. General layout improvements included. Requires cvslog2web from Changeset #145 (which will be committed shortly; the checked out copies do not sit in locations that make an atomic checkin easy and at this stage atomicity is not that important).

--- rool/rails/rcvsweb/trunk/config/routes.rb
+++ rool/rails/rcvsweb/trunk/config/routes.rb
@@ -9,8 +9,7 @@ ActionController::Routing::Routes.draw do |map|
   # CVShistory: Route through the 'rcvshistory' controller's 'run' action
   map.connect PATH_PREFIX + '/history/*url', :controller => 'rcvshistory', :action => 'run'
 
-  # Revision list
-  map.connect PATH_PREFIX + '/revisions/', :controller => 'revisions', :action => 'list'
+  # Synthesised revision list and cvslog2web wrapping
+  map.connect PATH_PREFIX + '/revisions/',        :controller => 'revisions', :action => 'list'
   map.connect PATH_PREFIX + '/revisions/:action', :controller => 'revisions'
-
 end
