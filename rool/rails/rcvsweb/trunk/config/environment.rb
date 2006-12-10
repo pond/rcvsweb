1a651d5bd046cd77eb574f3427e2882d972ad643
rool <rool@fbfdfdf0-2b19-0410-bbe2-c4a1547f2368>
2006-12-10 19:28:59 +0000

    Reconfigured all applications for use in the ROOL account with its new layout. All files are set up for the development branch of the filesystem. Caught up in this changeset is an unrelated modification to the directory listing routine in Radiant, which now sorts the entries alphabetically.

--- rool/rails/rcvsweb/trunk/config/environment.rb
+++ rool/rails/rcvsweb/trunk/config/environment.rb
@@ -14,10 +14,22 @@ require File.join(File.dirname(__FILE__), 'boot')
 # URLs (i.e. according to the web server configuration, not the
 # filesystem location) and location in the filesystem, rather than
 # according to the Web server, of the CVSweb and CVShistory scripts.
+# Next up is the filesystem location of CVSLog2Web output and the
+# path to use for URLs for CVShistory - the bit up to and including
+# the ".cgi", before the query string is appended, excluding the
+# host. The host is taken from ENV['SERVER_ADDR']. If the request
+# uses port DEVEL_HTTPS_PORT, it is switched to DEVEL_HTTP_PORT for
+# a normal HTTP request. If the port is already DEVEL_HTTP_PORT it
+# is left alone, again for a normal HTTP request. Otherwise, the
+# port is reset to 80.
 
-PATH_PREFIX         = '/rails/rcvsweb'
-CVSWEB_LOCATION     = '/home/adh/perl/cvsweb/cvsweb.cgi'
-CVSHISTORY_LOCATION = '/home/adh/python/cvshistory/cvshistory.cgi'
+PATH_PREFIX         = ENV['RAILS_RELATIVE_URL_ROOT']
+CVSWEB_LOCATION     = '/home/rool/devel/perl/cvsweb/cvsweb.cgi'
+CVSHISTORY_LOCATION = '/home/rool/devel/python/cvshistory/cvshistory.cgi'
+CVSLOG2WEB_OUTPUT   = '/home/rool/devel/python/cvslog2web/public'
+CVSLOG2WEB_PREFIX   = '/python/cvshistory/cvshistory.cgi'
+DEVEL_HTTP_PORT     = '25080'
+DEVEL_HTTPS_PORT    = '25081'
 
 Rails::Initializer.run do |config|
   # We don't run in the document root, so images etc. must come from
