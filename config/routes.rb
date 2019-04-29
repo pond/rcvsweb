ad7acfd5513f380e79b6c7aaff303094b96acd70
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2019-04-29 23:00:21 +1200

    Routing fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'application#index'
 
-  get    'view/*url', to:     'rcvsweb#run'
-  get 'history/*url', to: 'rcvshistory#run'
+  get    'view/(*url)', to:     'rcvsweb#run'
+  get 'history/(*url)', to: 'rcvshistory#run'
 
   get 'revisions',         to: 'revisions#list'
   get 'revisions/:action', controller: 'revisions'
